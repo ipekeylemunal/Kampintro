0a7e19faa260c1732b184a056e5cdf93a046cf06
ipek ünal <ipek-eylem@hotmail.com>
2021-01-23 23:58:22 +0300

    23 ocak

---
+++ Oop2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "1236459";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "4564";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "465465";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(musteri1);
+            customerManager.Ekle(musteri2);
+
+
+
+        }
+    }
+}
